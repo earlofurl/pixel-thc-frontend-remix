d90ad7cd147dd697953dfbb4c0874dc172081156
earlofurl <jackthename@gmail.com>
2022-12-31 22:00:27 -0800

    Minor fix

--- app/auth.server.ts
+++ app/auth.server.ts
@@ -1,55 +1,54 @@
-import { Authenticator } from 'remix-auth'
-import { FormStrategy } from 'remix-auth-form'
-import invariant from 'tiny-invariant'
-import type { SessionObject } from '~/services/session.server'
-import { sessionStorage } from '~/services/session.server'
+import { Authenticator } from 'remix-auth';
+import { FormStrategy } from 'remix-auth-form';
+import invariant from 'tiny-invariant';
+import type { SessionObject } from '~/services/session.server';
+import { sessionStorage } from '~/services/session.server';
 
 // TODO: I think maybe I should be passing the session cookie back and forth and parsing on backend.
 
-export const authenticator = new Authenticator<SessionObject>(sessionStorage)
+export const authenticator = new Authenticator<SessionObject>(sessionStorage);
 
 authenticator.use(
-	new FormStrategy(async ({ form, context }) => {
-		// Here you can use `form` to access and input values from the form.
-		// and also use `context` to access more things from the server
-		let email = form.get('email') as string // or email... etc
-		let password = form.get('password') as string
+  new FormStrategy(async ({ form, context }) => {
+    // Here you can use `form` to access and input values from the form.
+    // and also use `context` to access more things from the server
+    let email = form.get('email') as string; // or email... etc
+    let password = form.get('password') as string;
 
-		// You can validate the inputs however you want
-		invariant(typeof email === 'string', 'email must be a string')
-		invariant(email.length > 0, 'email must not be empty')
+    // You can validate the inputs however you want
+    invariant(typeof email === 'string', 'email must be a string');
+    invariant(email.length > 0, 'email must not be empty');
 
-		invariant(typeof password === 'string', 'password must be a string')
-		invariant(password.length > 0, 'password must not be empty')
+    invariant(typeof password === 'string', 'password must be a string');
+    invariant(password.length > 0, 'password must not be empty');
 
-		// TODO: First test auth flow without hashed password in frontend. Then add hashing to password.
-		// And if you have a password you should hash it
-		// let hashedPassword = await hash(password)
+    // TODO: First test auth flow without hashed password in frontend. Then add hashing to password.
+    // And if you have a password you should hash it
+    // let hashedPassword = await hash(password)
 
-		const bodyObject = JSON.stringify({ email, password })
+    const bodyObject = JSON.stringify({ email, password });
 
-		// And finally, you can find, or create, the user
-		const userResponse = await fetch(
-			`${process.env.API_BASE_URL}/users/login`,
-			// 'http://localhost:8088/users/login',
-			{
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: bodyObject,
-			},
-		)
+    // And finally, you can find, or create, the user
+    const userResponse = await fetch(
+      `${process.env.API_BASE_URL}/users/login`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: bodyObject,
+      },
+    );
 
-		// Get the Set-Cookie header from the response
-		const sessionCookie = await sessionStorage.getSession(
-			userResponse.headers.get('Set-Cookie'),
-		)
+    // Get the Set-Cookie header from the response
+    const sessionCookie = await sessionStorage.getSession(
+      userResponse.headers.get('Set-Cookie'),
+    );
 
-		// Set the sessionCookie in sessionStorage
-		await sessionStorage.commitSession(sessionCookie)
+    // Set the sessionCookie in sessionStorage
+    await sessionStorage.commitSession(sessionCookie);
 
-		return userResponse.json()
-	}),
-	'email-pass',
-)
+    return userResponse.json();
+  }),
+  'email-pass',
+);
